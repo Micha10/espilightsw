77df97bb4bb39b609853a29585f1e38f646f386a
m <mikado@localdomain.local>
2023-06-09 10:51:55 +0200

    Merge cpp into h

--- components/espilightsw/espilightsw.h
+++ components/espilightsw/espilightsw.h
@@ -10,6 +10,8 @@
 namespace esphome {
 namespace espilightsw {
 
+static const char *const TAG = "espilightsw_switch";
+
 
 /// This class implements calculation of absolute humidity from temperature and relative humidity.
 class EspilightComponent : public switch_::Switch, public Component {
@@ -19,19 +21,38 @@ class EspilightComponent : public switch_::Switch, public Component {
   void set_protocol_name(String protocol_name) { this->protocol_name_ = protocol_name; }
   void set_protocol_data(String protocol_data) { this->protocol_data_ = protocol_data; }
   void set_pin(int pin) { pin_ = pin; }
-
-
-  void setup() override;
-  void dump_config() override;
+ 
+  void dump_config() {
+    LOG_SWITCH("", "Espilight Switch", this); 
+}
   float get_setup_priority() const override;
   //void loop() override;
   switch_::Switch *source_{nullptr};
 
   ESPiLight *pespilight{nullptr};
 
+void setup() {
+  ESP_LOGCONFIG(TAG, "Espilight '%s'...", this->get_name().c_str());
+}
+
  protected:
-  void do_it();
-  void write_state(bool state) override;
+  void do_it(void) {
+    if (pespilight == nullptr) {
+        pespilight = new ESPiLight(pin_);
+    }
+    
+    pespilight->send(this->protocol_name_, this->protocol_data_);    
+}
+float get_setup_priority() const { return setup_priority::DATA; }
+
+void write_state(bool state) {
+  if (state) {
+    source_->turn_on();
+    do_it();
+  } else {
+    source_->turn_off();
+  }
+}
 
   String protocol_name_ = "";
   String protocol_data_ = "";
